4bab7118c70108af7e31f258f9d2e5fe9e242f27
Jinkkuu <reddabomb82@gmail.com>
2024-07-20 23:20:22 -0400

    Make results screen look better

--- data/modules/resultsscreen.py
+++ data/modules/resultsscreen.py
@@ -8,8 +8,9 @@ from data.modules.settings import getsetting
 gradecolour=(114, 180, 181),(114, 142, 179),(105, 173, 99),(113, 85, 173),(173, 136, 61),(168, 70, 50),(20,20,20)
 testing=0
 replaymen=0
+resulttxt='Ranking','Performance','Accuracy','Ranked Score'
 def beatres(screen,w,h):
-    if getactivity()==9:
+    if getactivity()==9 or testing:
         from data.modules.gameplay import points,accuracy,hits
         from data.modules.onlineapi import oldstats,issubmiting,level,totacc,totrank,totperf,totscore,prevrank
         screen.fill(maincolour[0])
@@ -44,7 +45,7 @@ def beatres(screen,w,h):
         pygame.draw.rect(screen,gc,pygame.Rect(scrop),border_radius=20)
         center_text(screen,gradet,(scrop[0],scrop[1]+5,scrop[2],scrop[3]),colour=(255,255,255),type='grade')
         center_text(screen,str(format(int(points/getmaxpoints()*(1000000*getmult())),',')),(scrop[0],scrop[1]+80,scrop[2],scrop[3]),colour=(255,255,255),type='grade')
-        center_text(screen,'pp - '+str(str(format(int(points),',')))+'/'+str(str(str(format(getmaxpoints(),',')))),(w//2-150,h//2-30,0,0),colour=(255,255,255))
+        center_text(screen,'pp - '+str(str(format(int(points),',')))+'/'+str(str(str(format(int(getmaxpoints()),',')))),(w//2-150,h//2-30,0,0),colour=(255,255,255))
         center_text(screen,str(accuracy)+'%',(w//2+160,h//2-30,0,0),colour=(255,255,255))
         center_text(screen,'MAX - '+str(pup[0]),(w//2-150,h//2,0,0),colour=(255,255,255))
         center_text(screen,'GREAT - '+str(pup[1]),(w//2+160,h//2,0,0),colour=(255,255,255))
@@ -53,15 +54,20 @@ def beatres(screen,w,h):
         if issubmiting and getsetting('username'):
             center_text(screen,'Submitting Score...',(scrop[0],scrop[1]+270,scrop[2],scrop[3]),colour=(255,255,255))
         elif getsetting('username'):
-            pygame.draw.rect(screen,maincolour[1],pygame.Rect(w//2-110,h//2+43,220,35),border_radius=10)
+            pygame.draw.rect(screen,maincolour[1],pygame.Rect(w//2-110,h//2+40,220,35),border_radius=10)
             center_text(screen,'You are Level '+str(format(level,','))+'!',(w//2,h//2+60,0,0),colour=(255,255,255))
-            center_text(screen,'Overall Rank - #'+str(format(totrank,',')),(w//2+160,h//2+95,0,0),colour=(255,255,255))
-            center_text(screen,'Overall Points - '+str(format(totperf,',')),(w//2-150,h//2+95,0,0),colour=(255,255,255))
-            center_text(screen,'Overall Accuracy - '+str(round(totacc,2))+'%',(w//2-150,h//2+135,0,0),colour=(255,255,255))
-            center_text(screen,'Ranked Score - '+str(format(totscore,',')),(w//2+160,h//2+135,0,0),colour=(255,255,255))
+#            center_text(screen,'Overall Rank - #'+str(),(w//2+160,h//2+95,0,0),colour=(255,255,255))
+#            center_text(screen,'Overall Points - '+str(),(w//2-150,h//2+95,0,0),colour=(255,255,255))
+#            center_text(screen,'Overall Accuracy - '+str()+'%',(w//2-150,h//2+135,0,0),colour=(255,255,255))
+#            center_text(screen,'Ranked Score - '+str(),(w//2+160,h//2+135,0,0),colour=(255,255,255))
+            queue='#'+format(totrank,','),format(totperf,','),str(round(totacc,2))+'%',format(totscore,',')
+            for a in range(0,4):
+                pygame.draw.rect(screen,maincolour[2],(w//2-210+(110*a),h//2+100,100,55),border_radius=10)
+                center_text(screen,resulttxt[a],(w//2-210+(110*a),h//2+100,100,25),type='min',colour=(255,255,255))
+                center_text(screen,queue[a],(w//2-210+(110*a),h//2+130,100,0),type='min',colour=(255,255,255))
             changed=[totrank-prevrank,totperf-oldstats[0],round(totacc-oldstats[2],2),totscore-oldstats[1],level-oldstats[3]]
             final=[totrank,totperf,totacc,totscore,level]
-            klap=(w//2+200,h//2+105),(w//2-121,h//2+105),(w//2-109,h//2+145),(w//2+160,h//2+145),(w//2+45,h//2+68,0,0)
+            klap=(w//2-210+(110*0),h//2+145,100,0),(w//2-210+(110*1),h//2+145,100,0),(w//2-210+(110*2),h//2+145,100,0),(w//2-210+(110*3),h//2+145,100,0),(w//2,h//2+90,0,0)
             for a in range(1,len(changed)+1):
                 cha=(255, 155, 128),(186, 255, 171)
                 if changed[a-1]<0:
@@ -86,11 +92,14 @@ def beatres(screen,w,h):
                     if a == 1:
                         if chac==cha[1]:
                             fip=fip.replace('-','+')
-                    screen.blit(getfonts(0).render(chap+str(fip)+suf,True,chac),klap[a-1])
+                    if a == 5:
+                        bo=''
+                    else:
+                        bo='min'
+                    center_text(screen,chap+str(fip)+suf,klap[a-1],type=bo,colour=chac)
         else:
             center_text(screen,'Hey, Your not Logged in yet!',(scrop[0],scrop[1]+270,scrop[2],scrop[3]))
             center_text(screen,'You can compete if you Log in~',(scrop[0],scrop[1]+290,scrop[2],scrop[3]))
-
         screen.blit(getfonts(0).render(title,True,(255,255,255)),(20,20))
         butt=draw_button(screen,((scrop[0]-90,scrop[1]+360,scrop[2]*3-20,scrop[3]),),('Continue',))
         if butt:
