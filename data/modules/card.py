d4aad02d5cb59f41b0109bbc6ed4c3ff001fd656
Jinkkuu <reddabomb82@gmail.com>
2024-07-25 16:32:49 -0400

    Change Ranking font

--- data/modules/card.py
+++ data/modules/card.py
@@ -1,7 +1,7 @@
 import pygame,time,sys
 from data.modules.renderapi import getfonts
 from data.modules.colours import maincolour
-def main(screen,pos,points=0,score=0,level=0,rank=0,accuracy=69,username=None,mini=False):
+def main(screen,pos,points=0,score=0,overidecolour=0,hidebg=0,level=0,rank=0,accuracy=69,username=None,mini=False):
     ticker=time.time()
     p=points
     r=rank
@@ -9,14 +9,18 @@ def main(screen,pos,points=0,score=0,level=0,rank=0,accuracy=69,username=None,mi
     rank = format(rank,',')
     level = format(level,',')
     score = format(score,',')
-    blend=maincolour[5][0]-20,maincolour[5][1]-20,maincolour[5][2]-20
+    if not overidecolour:
+        blend=maincolour[5][0]-20,maincolour[5][1]-20,maincolour[5][2]-20
+    else:
+        blend=overidecolour
     if mini:
         height=50
     else:
         height=70
-    pygame.draw.rect(screen,maincolour[5],pygame.Rect(pos[0],pos[1],300,height),border_radius=10)
+    if not hidebg:
+        pygame.draw.rect(screen,maincolour[5],pygame.Rect(pos[0],pos[1],300,height),border_radius=10)
     if r>0 and p>0:
-        t=getfonts(2).render(f'#{rank}',True,blend)
+        t=getfonts(4).render(f'#{rank}',True,blend)
         rtl=t.get_rect()
         rtl=pos[0]+290-rtl[2],pos[1]+height-50
         screen.blit(t,rtl[:2])
